45011b3daa4147c3e13f0ede7dab7492b9726e48
TheoKergaravat <theokergaravat@gmail.com>
2022-11-23 15:28:33 +0100

    ajout texte 3 monstres

--- monScript.js
+++ monScript.js
@@ -4,6 +4,6 @@ afficheAction = document.getElementById("afficheAction");
 
 maDiv.onclick = function() {
     valeurResultat.innerHTML = parseInt(valeurResultat.innerHTML)-10;
-    afficheAction.innerHTML = "le monstre à perdu 10PV !";
+    afficheAction.innerHTML = "le minotaure à perdu 10PV !";
 }
 
